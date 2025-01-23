a7931f184256add257b60cfd08857375767b78c9
Haiqal Mochammad Satria Utama <haiqal231@gmail.com>
2025-01-23 15:43:13 +0700

    add ternary operator

--- test-playground.js
+++ test-playground.js
@@ -5,3 +5,7 @@ console.log(dec); // 31
 let decimal = 20
 let hexa = decimal.toString(16)
 console.log(hexa); // 14
+
+const suhu = 40;
+const kondisiCuaca = suhu > 30 ? 'Suhu panas' : 'Suhu dingin';
+console.log(kondisiCuaca);
